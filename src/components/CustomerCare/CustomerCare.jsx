9a1b31782e67fe579a6482afa8f752aee4003006
Suagrcothe <ifeanyivalentine82@gmail.com>
2023-08-12 18:27:25 +0100

    customer care chat bot

--- src/components/CustomerCare/CustomerCare.jsx
+++ src/components/CustomerCare/CustomerCare.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./CustomerCare.css"; // We'll generate this CSS in the next step
+import Footbar from "../Footbar/Footbar";
 
-const CustomerCare = () => {
+function CustomerCare() {
+  const [messages, setMessages] = useState([
+    { text: "Hello!", sender: "user" },
+    {
+      text: "Hi there!, how can we help you, ticket for this chat is #3435627",
+      sender: "bot",
+    },
+  ]);
+  const [inputText, setInputText] = useState("");
+
+  const handleSendMessage = () => {
+    if (inputText.trim() === "") return;
+
+    const newMessage = { text: inputText, sender: "user" };
+    setMessages([...messages, newMessage]);
+    setInputText(""); // Clear the input field
+
+    // Simulate bot's response after a short delay
+    setTimeout(() => {
+      const botResponse = {
+        text: "Thank you for contacting us, our technicians are currently on their way to assist you",
+        sender: "bot",
+      };
+      setMessages([...messages, botResponse]);
+    }, 1000);
+  };
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
   return (
-    <div>CustomerCare</div>
-  )
+    <>
+      <div className="chat-container">
+        <div className="chat-messages">
+          {messages.map((message, index) => (
+            <div key={index} className={`message ${message.sender}`}>
+              {message.text}
+            </div>
+          ))}
+        </div>
+        <div className="input-container">
+          <textarea
+            type="text"
+            placeholder="Type you issue..."
+            value={inputText}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleSendMessage}>Send</button>
+        </div>
+      </div>
+      <Footbar />
+    </>
+  );
 }
 
-export default CustomerCare
+export default CustomerCare;
